928bd6b1b1b9010a617c7522cf94163549d2ee2a
Gourav <chauhang101101@gmail.com>
2025-08-30 14:37:49 +0530

    Final Project

--- kirana-backend/src/main/java/com/kirana/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kirana.repository;
-
-import com.kirana.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-    // Additional query methods if needed
-}
-
